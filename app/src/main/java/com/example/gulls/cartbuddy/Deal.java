5bc0d30d4593ac4b27b9fd2837cd1fa950fe1ad5
Yuanrui <yuanruiren@outlook.com>
2017-11-29 13:49:50 -0600

    Deal

---
+++ app/src/main/java/com/example/gulls/cartbuddy/Deal.java
@@ -0,0 +1,23 @@
+package com.example.gulls.cartbuddy;
+
+/**
+ * Created by Yuanrui on 11/29/17.
+ */
+import android.location.Location;
+
+import java.util.Date;
+
+public class Deal {
+    public int id;
+    public String user;
+    public String photoUrl;
+    public String title;
+    public String description;
+    public int likes;
+    public Date date;
+    public String category;
+    public Location location;
+    public Deal(){
+        
+    }
+}
