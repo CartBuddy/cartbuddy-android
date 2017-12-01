cf76eaa5c39927964a8acdcea788b06070265fe6
Yuanrui <yuanruiren@outlook.com>
2017-12-01 00:10:10 -0600

    search

--- app/src/main/java/com/example/gulls/cartbuddy/MainActivity.java
+++ app/src/main/java/com/example/gulls/cartbuddy/MainActivity.java
@@ -7,15 +7,11 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.GridView;
 import android.widget.ListView;
-import android.widget.SearchView;
 import android.widget.Toast;
+import android.support.v7.widget.Toolbar;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -23,22 +19,36 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.miguelcatalan.materialsearchview.MaterialSearchView;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.ListViewCompat;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.miguelcatalan.materialsearchview.MaterialSearchView;
+
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private final String TAG = "MAIN";
     final String serverUrl = "";
     Intent intent;
     ListView listView;
     ArrayList<Deal> deals;
-    DealAdapter dealAdapter;
+    MaterialSearchView searchView;
 
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
@@ -72,7 +82,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     };
 
 
-     void getDeals(String url, final Context context){
+    void getDeals(String url, final Context context) {
         // Instantiate the RequestQueue.
         RequestQueue queue = Volley.newRequestQueue(this);
         // Request a string response from the provided URL.
@@ -80,7 +90,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
-                        try{
+                        try {
                             JSONArray dealsJson = new JSONArray(response);
                             deals.clear();
                             for (int i = 0; i < dealsJson.length(); i++) {
@@ -88,9 +98,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                                 Deal d = new Deal(deal.getString("title"), deal.getString("photoUrl"), deal.getString("des"));
                                 deals.add(d);
                             }
-                            dealAdapter = new DealAdapter(context, deals);
-                            listView.setAdapter(dealAdapter);
-                        }catch(JSONException e){
+                            listView.setAdapter(new DealAdapter(context, deals));
+                        } catch (JSONException e) {
                             e.printStackTrace();
                         }
 
@@ -109,16 +118,82 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("CartBuddy");
+        toolbar.setTitleTextColor(Color.parseColor("#FFFFFF"));
+
         listView = (ListView) findViewById(R.id.list_view);
 //        getDeals(serverUrl, MainActivity.this);
         deals = new ArrayList<>();
-        deals.add(new Deal("t1","http://i.imgur.com/DvpvklR.png", "d1"));
-        deals.add(new Deal("t2","http://i.imgur.com/DvpvklR.png", "d2"));
-        deals.add(new Deal("t3","http://i.imgur.com/DvpvklR.png", "d3"));
+        deals.add(new Deal("t1", "http://i.imgur.com/DvpvklR.png", "d1"));
+        deals.add(new Deal("t2", "http://i.imgur.com/DvpvklR.png", "d2"));
+        deals.add(new Deal("t3", "http://i.imgur.com/DvpvklR.png", "d3"));
+        deals.add(new Deal("t4", "http://i.imgur.com/DvpvklR.png", "d1"));
+        deals.add(new Deal("t5", "http://i.imgur.com/DvpvklR.png", "d2"));
+        deals.add(new Deal("t6", "http://i.imgur.com/DvpvklR.png", "d3"));
+        deals.add(new Deal("t7", "http://i.imgur.com/DvpvklR.png", "d1"));
+        deals.add(new Deal("t8", "http://i.imgur.com/DvpvklR.png", "d2"));
+        deals.add(new Deal("t9", "http://i.imgur.com/DvpvklR.png", "d3"));
+        deals.add(new Deal("t10", "http://i.imgur.com/DvpvklR.png", "d1"));
+        deals.add(new Deal("t11", "http://i.imgur.com/DvpvklR.png", "d2"));
+        deals.add(new Deal("t12", "http://i.imgur.com/DvpvklR.png", "d3"));
         listView.setAdapter(new DealAdapter(MainActivity.this, deals));
+        searchView = (MaterialSearchView) findViewById(R.id.search_view);
+
+        searchView.setOnSearchViewListener(new MaterialSearchView.SearchViewListener() {
+            @Override
+            public void onSearchViewShown() {
+
+            }
+
+            @Override
+            public void onSearchViewClosed() {
+
+                //If closed Search View , lstView will return default
+                listView = (ListView) findViewById(R.id.list_view);
+                listView.setAdapter(new DealAdapter(MainActivity.this, deals));
+            }
+        });
+
+        searchView.setOnQueryTextListener(new MaterialSearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                if (newText != null && !newText.isEmpty()) {
+                    ArrayList<Deal> lstFound = new ArrayList<>();
+                    for (Deal item : deals) {
+                        if (item.title.toLowerCase().contains(newText.toLowerCase()))
+                            lstFound.add(item);
+                    }
+
+                    listView.setAdapter(new DealAdapter(MainActivity.this, lstFound));
+
+                } else {
+                    listView.setAdapter(new DealAdapter(MainActivity.this, deals));
+
+                }
+                return true;
+            }
+
+        });
+
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         BottomNavigationViewHelper.disableShiftMode(navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_item, menu);
+        MenuItem item = menu.findItem(R.id.action_search);
+        searchView.setMenuItem(item);
+        return true;
     }
 
     @Override
