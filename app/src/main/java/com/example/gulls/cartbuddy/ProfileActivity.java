bf221b2f444ff6ab52272e1308eda57eb341c269
Yuanrui <yuanruiren@outlook.com>
2017-12-03 17:51:52 -0600

    add navigation  to profile

--- app/src/main/java/com/example/gulls/cartbuddy/ProfileActivity.java
+++ app/src/main/java/com/example/gulls/cartbuddy/ProfileActivity.java
@@ -1,13 +1,53 @@
 package com.example.gulls.cartbuddy;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class ProfileActivity extends AppCompatActivity {
+    private Intent intent;
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    intent = new Intent(ProfileActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    return true;
+                case R.id.navigation_nearby:
+                    intent = new Intent(ProfileActivity.this, NearbyActivity.class);
+                    startActivity(intent);
+                    return true;
+                case R.id.navigation_popular:
+                    intent = new Intent(ProfileActivity.this, PopularActivity.class);
+                    startActivity(intent);
+                    return true;
+                case R.id.navigation_checklist:
+                    intent = new Intent(ProfileActivity.this, ChecklistActivity.class);
+                    startActivity(intent);
+                    return true;
+            }
+            return false;
+        }
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        //navigation
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        BottomNavigationViewHelper.disableShiftMode(navigation);
+        navigation.getMenu().getItem(4).setChecked(true);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
+
+
 }
